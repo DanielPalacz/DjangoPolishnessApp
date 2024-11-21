eb734c926f865af5554a02170e0acd361c90f2af
Daniel <daniel.palacz@gmail.com>
2024-11-22 00:21:54 +0100

    adds visual change on main page, polish flag as button. Also adds cosmetic styling changes for contact form.

--- polishness/forms.py
+++ polishness/forms.py
@@ -4,6 +4,17 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label="Imię")
-    email = forms.EmailField(required=True, label="Email")
-    message = forms.CharField(widget=forms.Textarea, required=True, label="Wiadomość")
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Imię/nick",
+        widget=forms.TextInput(attrs={"title": "Proszę podaj swoje imię lub nick."}),
+        error_messages={"required": "Proszę podaj swoje imię lub nick."},
+    )
+    email = forms.EmailField(
+        required=True,
+        label="Email",
+        widget=forms.TextInput(attrs={"title": "Proszę podaj poprawny adres e-mail."}),
+        error_messages={"required": "Proszę podać adres e-mail.", "invalid": "Wprowadź poprawny adres e-mail."},
+    )
+    message = forms.CharField(widget=forms.Textarea(attrs={"title": ""}), required=True, label="Wiadomość")
